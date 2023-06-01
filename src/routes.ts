ed407f8ed2faac6c6a59ed3f114c6bc4b15b6d0b
Natanael Araújo <natanael@hotmail.co.jp>
2023-06-01 19:41:52 -0300

    Middleware de cliente autenticado

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateClientController } from './modules/clients/useCases/createClient/
 import { AuthenticateClientController } from './modules/accounts/authenticateClient/AuthenticateClientController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 import { CreateDeliveriesController } from './modules/deliveries/useCases/CreateDeliveriesController';
+import { ensureAuthenticateClient } from './middlewares/ensureAuthenticateClient';
 
 const routes = Router();
 
@@ -14,6 +15,6 @@ const createDeliveriesController = new CreateDeliveriesController();
 routes.post("/client", createClientController.handle);
 routes.post("/authenticate", authenticateClientController.handle);
 routes.post("/deliveryman", createDeliverymanController.handle);
-routes.post("/delivery", createDeliveriesController.handle);
+routes.post("/delivery", ensureAuthenticateClient ,createDeliveriesController.handle);
 
 export { routes };
