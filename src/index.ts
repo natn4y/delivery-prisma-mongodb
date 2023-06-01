9b6e86f9a1ae1beec8d3d24fc058dbb44f86cf34
Natanael Araújo <natanael@hotmail.co.jp>
2023-06-01 02:20:22 -0300

    prisma models schema

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,31 @@ async function createUser() {
       data: {
         name: "Admin",
         email: "admin@admin.com",
-        created_at: String(new Date),
-        isAdmin: false
+        created_at: new Date().toISOString(),
+        isAdmin: true
       },
     });
 
+    const deliveryman = await prisma.deliveryman.create({
+      data: {
+        username: "deliveryman",
+        password: "123456",
+      },
+    })
+
+    const deliveries = await prisma.deliveries.create({
+      data: {
+        id_client: '???',
+        id_deliveryman: '???',
+        item_name: 'Vatapá c/ pimenta',
+        create_at: '2023-06-01T05:11:54.447Z',
+        end_at: '2023-06-01T05:11:54.447Z'
+      }
+    })
+
     console.log("Usuário criado:", user);
+    console.log("Deliveryman criado:", deliveryman);
+
   } catch (error) {
     console.error("Erro ao criar usuário:", error);
   } finally {
