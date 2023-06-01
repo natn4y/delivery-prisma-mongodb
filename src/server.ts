f29d4310fa2f1da07c9ecd5a55870d37e521776a
Natanael Araújo <natanael@hotmail.co.jp>
2023-06-01 09:30:34 -0300

    Documentação com JSdoc no arquivo server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,12 +13,16 @@ app.listen(3001, () => {
   const ipAddress = getContainerIPAddress();
 
   if (ipAddress) {
-    console.log(`\n\n\n [System] - Server is running at http://${ipAddress}:3001\n\n\n`);
+    console.log(`\n\n\nServer is running at http://${ipAddress}:3001\n\n\n`);
   } else {
     console.error('Unable to find the IP address of the container.');
   }
 });
 
+/**
+ * Função que retorna o endereço IP do contêiner, se encontrado.
+ * @returns {string|null} O endereço IP do contêiner, ou null se não encontrado.
+ */
 function getContainerIPAddress() {
   const networkInterfaces = os.networkInterfaces();
 
@@ -34,6 +38,17 @@ function getContainerIPAddress() {
   return null;
 }
 
-function isContainerInterface(interfaceInfo: any) {
+/**
+ * Função auxiliar para verificar se uma interface de rede é a do contêiner.
+ *
+ * @param {Object} interfaceInfo - Informações da interface de rede. Veja a descrição abaixo para os detalhes das propriedades.
+ * @returns {boolean} Retorna true se a interface não é interna (do tipo loopback) e pertence à família de endereços IPv4, caso contrário retorna false.
+ *
+ * @description
+ * Informações da interface de rede:
+ * - `internal` (boolean): Indica se a interface é interna (do tipo loopback) ou não.
+ * - `family` (string): A família de endereços IP da interface de rede. No contexto deste código, representa o tipo de protocolo IP utilizado (por exemplo, "IPv4" ou "IPv6").
+ */
+function isContainerInterface(interfaceInfo: { internal: boolean, family: string }) {
   return !interfaceInfo.internal && interfaceInfo.family === 'IPv4';
 }
