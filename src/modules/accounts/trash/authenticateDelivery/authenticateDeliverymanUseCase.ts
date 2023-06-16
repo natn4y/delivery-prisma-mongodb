f492c9d317bf628aa4f02af62006d5c0daf193e7
Natanael Araújo <natanael@hotmail.co.jp>
2023-06-16 02:54:14 -0300

    Códigos antigos comentados

--- src/modules/accounts/trash/authenticateDelivery/authenticateDeliverymanUseCase.ts
+++ src/modules/accounts/trash/authenticateDelivery/authenticateDeliverymanUseCase.ts
@@ -1,41 +1,41 @@
-import { compare } from 'bcrypt';
-import { prisma } from '../../../../database/prismaClient';
-import { sign } from 'jsonwebtoken';
+// import { compare } from 'bcrypt';
+// import { prisma } from '../../../../database/prismaClient';
+// import { sign } from 'jsonwebtoken';
 
-interface IAuthenticateDeliveryman {
-  username: string;
-  password: string;
-}
+// interface IAuthenticateDeliveryman {
+//   username: string;
+//   password: string;
+// }
 
-class AuthenticateDeliverymanUseCase {
-  // Receber Dados
-  async execute({ username, password }: IAuthenticateDeliveryman) {
-    // Verificar se username está cadastrado
-    const deliveryman = await prisma.deliveryman.findFirst({
-      where: { username }
-    })
+// class AuthenticateDeliverymanUseCase {
+//   // Receber Dados
+//   async execute({ username, password }: IAuthenticateDeliveryman) {
+//     // Verificar se username está cadastrado
+//     const deliveryman = await prisma.deliveryman.findFirst({
+//       where: { username }
+//     })
 
-    if(!deliveryman) {
-      throw new Error("username or password invalid!");
-    }
+//     if(!deliveryman) {
+//       throw new Error("username or password invalid!");
+//     }
 
-    // Verificar se a senha corresponde ao username
-    const passwordMatch = await compare(password , deliveryman.password)
+//     // Verificar se a senha corresponde ao username
+//     const passwordMatch = await compare(password , deliveryman.password)
 
-    if(!passwordMatch) {
-      throw new Error("username or password invalid!");
-    }
+//     if(!passwordMatch) {
+//       throw new Error("username or password invalid!");
+//     }
 
-    //Gerar token
-    const token = sign({
-      username
-    }, "96a284ffc8e6fc5e8ee7671f070bf88118", {
-      subject: deliveryman.id,
-      expiresIn: "1d"
-    });
+//     //Gerar token
+//     const token = sign({
+//       username
+//     }, "96a284ffc8e6fc5e8ee7671f070bf88118", {
+//       subject: deliveryman.id,
+//       expiresIn: "1d"
+//     });
 
-    return token;
-  }
-}
+//     return token;
+//   }
+// }
 
-export { AuthenticateDeliverymanUseCase }
+// export { AuthenticateDeliverymanUseCase }
--- src/routes.ts
+++ src/routes.ts
@@ -1,44 +1,44 @@
-import { Router } from 'express';
-import { AuthenticateClientController } from './modules/accounts/authenticateClient/AuthenticateClientController';
-import { AuthenticateDeliverymanController } from './modules/accounts/trash/authenticateDelivery/authenticateDeliverymanController';
-import { CreateClientController } from './modules/clients/useCases/CreateClient/CreateClientController';
-import { FindAllDeliveriesController } from './modules/clients/useCases/trash/Deliveries/FindAllDeliveries/FindAllDeliveriesController';
-import { CreateDeliveriesController } from './modules/trash/deliveries/useCases/CreateDeliveries/CreateDeliveriesController';
-import { FindAllAvailableController } from './modules/trash/deliveries/useCases/FindDeliveries/FindAllAvailable/FindAllAvailableController';
-import { UpdateEndDateController } from './modules/trash/deliveries/useCases/UpdateEndDate/UpdateEndDateController';
-import { CreateDeliverymanController } from './modules/trash/deliveryman/useCases/CreateDeliveryman/CreateDeliverymanController';
-import { FindAllDeliveriesDeliverymanController } from './modules/trash/deliveryman/useCases/FindAllDeliveries/FindAllDeliveriesDeliverymanController';
-import { UpdateDeliverymanController } from './modules/trash/deliveryman/useCases/UpdateDelyveryman/UpdateDeliverymanController';
+// import { Router } from 'express';
+// import { AuthenticateClientController } from './modules/accounts/authenticateClient/AuthenticateClientController';
+// import { AuthenticateDeliverymanController } from './modules/accounts/trash/authenticateDelivery/authenticateDeliverymanController';
+// import { CreateClientController } from './modules/clients/useCases/CreateClient/CreateClientController';
+// import { FindAllDeliveriesController } from './modules/clients/useCases/trash/Deliveries/FindAllDeliveries/FindAllDeliveriesController';
+// import { CreateDeliveriesController } from './modules/trash/deliveries/useCases/CreateDeliveries/CreateDeliveriesController';
+// import { FindAllAvailableController } from './modules/trash/deliveries/useCases/FindDeliveries/FindAllAvailable/FindAllAvailableController';
+// import { UpdateEndDateController } from './modules/trash/deliveries/useCases/UpdateEndDate/UpdateEndDateController';
+// import { CreateDeliverymanController } from './modules/trash/deliveryman/useCases/CreateDeliveryman/CreateDeliverymanController';
+// import { FindAllDeliveriesDeliverymanController } from './modules/trash/deliveryman/useCases/FindAllDeliveries/FindAllDeliveriesDeliverymanController';
+// import { UpdateDeliverymanController } from './modules/trash/deliveryman/useCases/UpdateDelyveryman/UpdateDeliverymanController';
 
 
-const routes = Router();
+// const routes = Router();
 
-const authenticateClientController = new AuthenticateClientController();
-const authenticateDeliverymanController = new AuthenticateDeliverymanController();
-const createClientController = new CreateClientController();
-const createDeliverymanController = new CreateDeliverymanController();
-const createDeliveriesController = new CreateDeliveriesController();
-const findAllAvailableController = new FindAllAvailableController();
-const updateDeliverymanController = new UpdateDeliverymanController();
-const findAllDeliveriesController = new FindAllDeliveriesController();
-const findAllDeliveriesDeliverymanController = new FindAllDeliveriesDeliverymanController();
-const updateEndDateController = new UpdateEndDateController();
+// const authenticateClientController = new AuthenticateClientController();
+// const authenticateDeliverymanController = new AuthenticateDeliverymanController();
+// const createClientController = new CreateClientController();
+// const createDeliverymanController = new CreateDeliverymanController();
+// const createDeliveriesController = new CreateDeliveriesController();
+// const findAllAvailableController = new FindAllAvailableController();
+// const updateDeliverymanController = new UpdateDeliverymanController();
+// const findAllDeliveriesController = new FindAllDeliveriesController();
+// const findAllDeliveriesDeliverymanController = new FindAllDeliveriesDeliverymanController();
+// const updateEndDateController = new UpdateEndDateController();
 
-routes.post("/client/authenticate", authenticateClientController.handle);
-routes.post("/client", createClientController.handle);
+// routes.post("/client/authenticate", authenticateClientController.handle);
+// routes.post("/client", createClientController.handle);
 
-//routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
+// //routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle);
 
-//routes.post("/deliveryman", createDeliverymanController.handle);
+// //routes.post("/deliveryman", createDeliverymanController.handle);
 
-// routes.get("/delivery/available", ensureAuthenticateDeliveryman, findAllAvailableController.handle);
-// routes.get("/client/deliveries", ensureAuthenticateClient, findAllDeliveriesController.handle);
-// routes.get("/deliveries", ensureAuthenticateClient, findAllDeliveriesController.handle);
-// routes.get("/deliveryman/deliveries", ensureAuthenticateDeliveryman, findAllDeliveriesDeliverymanController.handle);
+// // routes.get("/delivery/available", ensureAuthenticateDeliveryman, findAllAvailableController.handle);
+// // routes.get("/client/deliveries", ensureAuthenticateClient, findAllDeliveriesController.handle);
+// // routes.get("/deliveries", ensureAuthenticateClient, findAllDeliveriesController.handle);
+// // routes.get("/deliveryman/deliveries", ensureAuthenticateDeliveryman, findAllDeliveriesDeliverymanController.handle);
 
-// routes.post("/delivery", ensureAuthenticateClient, createDeliveriesController.handle);
+// // routes.post("/delivery", ensureAuthenticateClient, createDeliveriesController.handle);
 
-// routes.put("/delivery/updateEndDate/:id/", ensureAuthenticateDeliveryman , updateEndDateController.handle);
-// routes.put("/delivery/updateDeliveryman/:id/", ensureAuthenticateDeliveryman , updateDeliverymanController.handle);
+// // routes.put("/delivery/updateEndDate/:id/", ensureAuthenticateDeliveryman , updateEndDateController.handle);
+// // routes.put("/delivery/updateDeliveryman/:id/", ensureAuthenticateDeliveryman , updateDeliverymanController.handle);
 
-export { routes };
+// export { routes };
