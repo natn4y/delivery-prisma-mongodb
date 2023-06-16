d778a798a4caf6040a48ecc685da8b200d38425b
Natanael Araújo <natanael@hotmail.co.jp>
2023-06-16 03:33:55 -0300

    cors instalado e configurado

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import express, { Request, Response } from 'express';
 import morgan from 'morgan'
 import os from 'os';
 import salaoRoutes from './routes/salao.routes';
-
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors)
 app.use(express.json())
-app.use('/salao', salaoRoutes);
 app.use(morgan('dev'))
+
+app.use('/salao', salaoRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   return res.json({
